0f6abdc0e7e1cec7dacc35dbdcc56751828a40f6
RaghavGupta-01 <raghav010304@gmail.com>
2024-04-18 21:51:59 +0530

    added pagination & modified redirect routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.js';
 import Home from './components/Home/Home.js';
 import Auth from './components/Auth/Auth.js';
+import PostDetails from './components/PostDetails/PostDetails';
 
-const App = () => { 
-  return(
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+  return (
     <BrowserRouter>
       <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home/>} />
-          <Route path="/auth" exact element={<Auth/>} />
+          <Route path="/" exact element={<Navigate to="/posts" />} />
+          <Route path="/posts" exact element={<Home />} />
+          <Route path="/posts/search" exact element={<Home />} />
+          <Route path="/posts/:id" exact element={PostDetails} />
+          <Route path="/auth" exact element={!user ? <Auth /> : <Navigate to="/posts" replace/>} />
         </Routes>
         {/* <Home /> */}
       </Container>
