78424ce4c23daaa49801948b59fa0ef5afd4cdac
RaghavGupta-01 <raghav010304@gmail.com>
2025-03-13 20:43:28 +0530

    changed origin

--- server/index.js
+++ server/index.js
@@ -15,15 +15,15 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://travelgram-app.vercel.app"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
-  };
-  
-  app.use(cors(corsOptions));
-  app.options("*", cors(corsOptions));  
-  
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 
 app.use('/posts', postRoutes);       //this will make sure that all the routes in post routes will start from /posts
 app.use('/user', userRoutes);
@@ -31,7 +31,7 @@ app.use('/user', userRoutes);
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true,retryWrites: true, w: 'majority' })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, retryWrites: true, w: 'majority' })
     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error));
 
